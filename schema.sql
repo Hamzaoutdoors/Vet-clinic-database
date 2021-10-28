6360f7d16198327674882c8fdf5acbc89d6afc00
hamzaoutdoors <ellaouzihamza@gmail.com>
2021-10-28 18:01:45 +0100

    feat : add more table to bd

--- schema.sql
+++ schema.sql
@@ -9,3 +9,17 @@ CREATE TABLE animals (
 	weight_kg numeric NOT NULL,
 	species VARCHAR(50)
 );
+
+CREATE TABLE owners (
+    owner_id SERIAL,
+	full_name VARCHAR(100) NOT NULL,
+	age INT NOT NULL,
+	PRIMARY KEY (id_owner)
+);
+
+CREATE TABLE species (
+    specie_id SERIAL,
+	name VARCHAR(100) NOT NULL,
+	PRIMARY KEY (specie_id)
+);
+
